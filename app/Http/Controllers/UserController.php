86fcf930c50edc4cdc90a31e06842b84d29ccc46
seamlessOlanrewaju <olanrewaju@seamlesshr.com>
2021-12-08 23:17:55 +0100

    added all files for the first spa in educative

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class UserController extends Controller
 {
     public function store (Request $request)
     {
         $requestBody = [
-            'name' => 'required|unique:posts', 
+            'name' => 'required', 
             'point' => 'nullable|integer',  
         ];
 
-        $request->name = '';
-        $request->point = 'My point';
+        $request['name'] = '';
+        $request['point'] = 'My point';
+
 
         $validator = Validator::make($request->all(), $requestBody);
 
         if ($validator->fails()) {
-            print_r($validator);
+            print_r($validator->getMessageBag()->toArray());
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\UserController;
 
 
 
